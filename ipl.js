77436011797c5a966fc6eba8102e23040fecb5bc
kishan reddy <kishanreddy95@gitlab.com>
2018-08-31 18:16:32 +0530

    Server set

--- ipl.js
+++ ipl.js
@@ -1,10 +1,32 @@
-var assert = chai.assert;
+const csvPath = "./ipl/matches.csv";
+const csv = require('csvtojson');
+const fs = require('fs');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(4));
+let iplObj = {};
+
+csv().fromFile(csvPath).then(function(jsonObj) {
+
+    jsonObj.forEach((val) => {
+      if(iplObj.hasOwnProperty(val.season)) {
+        iplObj[val.season]++;
+      } else {
+        iplObj[val.season] = 1;
+      }
     });
-  });
+    console.log(iplObj);
+    fs.writeFile('ipl.json', JSON.stringify(iplObj), function() {});
+
+    return jsonObj;
 });
+
+
+// console.log(value);
+// function toCreateMatchesPerSeason(jsonObj) {
+//   console.log(jsonObj);
+// }
+
+
+
+
+
 
